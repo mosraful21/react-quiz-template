c523bd3e51b22c9d23ec8d078159c03f22c186aa
mosraful21 <mosraful21@gmail.com>
2022-10-11 19:24:37 +0600

    Ststistics added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,11 +3,15 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h3>Some Question in React</h3>
+            <br /><br />
             <h2 className='text-3xl'>What is the purpose of react router</h2>
             <p>Routing is a process in which a user is directed to different pages based on their action or request. ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.React Router is a standard library system built on top of the React and used to create routing in the React application using React Router Package. It provides the synchronous URL on the browser with data that will be displayed on the web page. It maintains the standard structure and behavior of the application and mainly used for developing single page web applications. React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
             <br />
-            <h2 className='text-3xl'></h2>
+            <h2 className='text-3xl'>How does context API works</h2>
+            <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux. React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. </p>
+            <br />
+            <h2 className='text-3xl'>What is useRaf hook</h2>
+            <p>The useRef Hook is a function that returns a mutable ref object whose .current property is initialized with the passed argument (initialValue). The returned object will persist for the full lifetime of the component. <br />const refContainer = useRef(initialValue); <br /> The useRef returns a mutable ref object. This object has a property called .current. The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The .current property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component. </p>
         </div>
     );
 };
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
 import './Item.css'
 
 const Item = ({ item }) => {
@@ -12,7 +13,10 @@ const Item = ({ item }) => {
                 <h2 className='text-2xl font-semibold'>Topic: {name}</h2>
                 <p><small>Total Question: {total}</small></p>
             </div>
-            <button className='bg-blue-400 rounded-lg px-24 py-1 font-semibold mt-2'>Start Now</button>
+            <div className='bg-blue-400 flex items-center justify-center rounded-lg px-24 py-1 font-semibold mt-2'>
+                <button onClick={''}>Start Now</button>
+                <ArrowRightIcon className='h-4 w-4 text-black-500 ml-2'></ArrowRightIcon>
+            </div>
         </div>
     );
 };
